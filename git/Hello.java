7ec0e78fc3577b76d4d8f6745dff3ace7ec20fd9
nickgabriel <nickgabriel.usa.net>
2017-05-27 16:25:41 -0400

    4th commit

--- git/Hello.java
+++ git/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
        public static void main(String[] args)  {
-              System.out.println("Hello, EVERYONE!");
+              System.out.println("Hello, world!");
+              System.out.println("Hello, Sophie!");
+              System.out.println("Hello, Bella!");
        }
 }
